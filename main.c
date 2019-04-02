95b230f59d47623003970222de8ffa0fcb151cbd
Inga Leinasars <inga.leinasars@uon.edu.au>
2019-04-02 02:09:49 +0000

    commit at end of week 6 lab

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+//write function to use to encrypt the message provided 
+int encryptCode(char *message);
 
 int main()
 {
+    const char message[100], c; //c is the character. links to ASCII 
+    int index; //index is the letter/number specific thing lol
+    int key; //key is the amount of rotation - Caesar Cipher
+    
+    printf("Enter a message using upper case letters to be encrypted: ");
+    printf("Enter a value for key: ");
+    scanf("%d", &key);
+    
+    //do something bro 
+    
+    //exit main()
+    
+    //do function 
+    //insert code to encrypt
+    //print encrypted code badabing badaboom
+    
+    // just experimenting with different codes 
+    const char message[20] ={"Hello World"};
+    printf("%d\n", message[4]);
+    
 
-
-  return 0;
+    
+    
+    return 0;
 }
