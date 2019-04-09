a4bec40dc32b024c6ecf387f072cde6709273108
Inga Leinasars <inga.leinasars@uon.edu.au>
2019-04-09 03:00:44 +0000

    end of lab week 7. worked out encrypt and decrypt codes separately without use of functions

--- main.c
+++ main.c
@@ -4,28 +4,49 @@ int encryptCode(char *message);
 
 int main()
 {
-    const char message[100], c; //c is the character. links to ASCII 
-    int index; //index is the letter/number specific thing lol
+    char message[100]; //
+    int index; 
     int key; //key is the amount of rotation - Caesar Cipher
     
     printf("Enter a message using upper case letters to be encrypted: ");
+    scanf("%s", &message);
     printf("Enter a value for key: ");
     scanf("%d", &key);
     
-    //do something bro 
+    printf("key is equal to %d\n", key);
+    printf("message to encrypt is %s\n", message);
+    
+    
+    //do something 
     
     //exit main()
+    return 0; 
+}
+
+int encryptCode(char *message)
+    {
+       
+ `for(int index = 0; message[index] !=0; index++)
+       {
+           if (message[index] > 64 && message[index] < 91)
+           {
+               message[index] = (message[index] + k)(%26)
+           }
+           
+           else 
+           {
+               
+           }
     
+      return message;     
+    }
     //do function 
     //insert code to encrypt
     //print encrypted code badabing badaboom
     
-    // just experimenting with different codes 
-    const char message[20] ={"Hello World"};
-    printf("%d\n", message[4]);
+    
+    
+// did decrypt and encrypt codes separately in separate projects. got them working. next step to implement as functions
+    
     
 
-    
-    
-    return 0;
-}
